78760ff438c0c361a893018651cebe660b86d911
Norman Kong <normankong@gmail.com>
2022-02-17 01:04:39 -0800

    Update with new result

--- Lab4/analytic/lab4xThread.py
+++ Lab4/analytic/lab4xThread.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 x = ["32", "64", "128", "256", "512", "1024"]    # Thread Number
 
 # Input the finding in order to plot the graph
-y1 = [1061, 1286, 1330, 2552, 5158, 10397]       # Mean Response Time
-y2 = [29.78, 48.11, 88.88, 93.61, 93.50, 91.08]  # TPS
+y1 = [1018.3697102864584,1025.806884765625, 1301.5977376302083,2553.0930989583335,5118.595784505208,10303.908854166666] # Mean Response Time
+y2 = [31.388817,62.207642,96.230804,96.624283,96.069050, 93.357544 ]  # TPS
+
+
 
 label1 = 'Mean Response'
 label2 = 'TPS'
