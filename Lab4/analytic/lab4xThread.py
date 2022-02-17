d473e85e9e876bd9a3f8bf0f6fe98aca1a17b55a
Norman Kong <normankong@gmail.com>
2022-02-17 01:05:38 -0800

    Update TPS/Mean data

--- Lab4/analytic/lab4xThread.py
+++ Lab4/analytic/lab4xThread.py
@@ -4,9 +4,7 @@ x = ["32", "64", "128", "256", "512", "1024"]    # Thread Number
 
 # Input the finding in order to plot the graph
 y1 = [1018.3697102864584,1025.806884765625, 1301.5977376302083,2553.0930989583335,5118.595784505208,10303.908854166666] # Mean Response Time
-y2 = [31.388817,62.207642,96.230804,96.624283,96.069050, 93.357544 ]  # TPS
-
-
+y2 = [31.388817,62.207642,96.230804,96.624283,96.069050, 93.357544]  # TPS
 
 label1 = 'Mean Response'
 label2 = 'TPS'
@@ -18,10 +16,10 @@ title1 = 'Mean Response vs Threads'
 title2 = 'TPS vs Threads'
 
 # Switch the information here
-y = y1
-title = title1
-label = label1
-ylabel = ylabel1
+y = y2
+title = title2
+label = label2
+ylabel = ylabel2
 
 plt.style.use('seaborn-whitegrid')
 plt.figure(figsize=(15,8))
