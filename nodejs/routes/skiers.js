90094d390d94aba4451c249cb2ddba93227409e5
Norman Kong <normankong@gmail.com>
2022-02-19 09:03:25 -0800

    Add Nodejs+Express

---
+++ nodejs/routes/skiers.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/:resortID/seasons/:seasonID/days/:dayID/skiers/:skierID', function (req, res, next) {
+    console.log(req.params);
+    res.status(201);
+    setTimeout(() => {
+        res.end();
+    }, 1000);
+});
+
+router.get('/:resortID/seasons/:seasonID/days/:dayID/skiers/:skierID', function (req, res, next) {
+    console.log(req.params);
+    res.status(200);
+    res.contentType("application/json");
+    res.end(req.params.skierID);
+});
+
+router.get('/:skierID/vertical', function (req, res, next) {
+    console.log(req.params);
+    let response = {
+        "resorts": [
+            {
+                "seasonID": `${req.params.skierID}`,
+                "totalVert": 0
+            }
+        ]
+    }
+    res.status(200);
+    res.contentType("application/json");
+    res.end(JSON.stringify(response));
+});
+
+module.exports = router;
